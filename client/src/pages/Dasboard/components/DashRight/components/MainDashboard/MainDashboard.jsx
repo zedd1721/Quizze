f621d4a41de94bdbbbd02ad7ba259e01c3b605ad
Zain Shaikh <szain2024@gmail.com>
2025-03-09 17:12:46 +0530

    MainDashboard

--- client/src/pages/Dasboard/components/DashRight/components/MainDashboard/MainDashboard.jsx
+++ client/src/pages/Dasboard/components/DashRight/components/MainDashboard/MainDashboard.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import styled from "./MainDashboard.module.css";
+import { IoEyeSharp } from "react-icons/io5";
 function MainDashboard() {
   return (
-    <div>MainDashboard</div>
-  )
+    <>
+      <div className={styled.mainTop}>
+        <div className={styled.quizCreated}>
+          <h1>
+            12 <span>Quiz</span>
+          </h1>
+          <h2>Created</h2>
+        </div>
+        <div className={styled.questionCreated}>
+          <h1>
+            110 <span>questions</span>
+          </h1>
+          <h2>Created</h2>
+        </div>
+        <div className={styled.totalImpression}>
+          <h1>
+            989 <span>Total</span>
+          </h1>
+          <h2>Impression</h2>
+        </div>
+      </div>
+      <div className={styled.mainBottom}>
+        <div className={styled.bottomHead}>
+          <h1>Trending Quiz</h1>
+        </div>
+        <div className={styled.bottomContent}>
+          <div className={styled.quizCard}>
+            <div className={styled.quizCardTop}>
+              <div className={styled.quizTitle}>Quiz 1</div>
+              <div className={styled.quizStats}>
+                <span className={styled.views}>
+                  667 
+                </span>
+                <IoEyeSharp className={styled.icon}/>
+              </div>
+            </div>
+            <p className={styled.quizDate}>Created on: 04 Sep, 2023</p>
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default MainDashboard
+export default MainDashboard;
